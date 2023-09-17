34b4343ddd0a000b7d7e6ed179df1970ca0f7af8
AKhilesh <kothapallyakhil123@gmail.com>
2023-09-17 23:24:52 +0530

    added ec2

--- ec2/server.tf
+++ ec2/server.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
