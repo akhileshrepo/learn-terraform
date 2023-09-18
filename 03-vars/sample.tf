345c26e8d6b100f59cb06168d62624b7406c4479
AKhilesh <kothapallyakhil123@gmail.com>
2023-09-18 19:54:40 +0530

    made changes

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -11,3 +11,66 @@ output "fruit_name" {
 variable "fruits" {
     default = ["apple", "banana", "Mango"]
 }
+
+
+# Map variable plain
+variable "fruit_stock"{
+    default = { apple = 100, banana = 209}
+}
+
+# Map variable, Map of maps
+variable "fruit_stock_with_price" {
+    default = {
+        apple = {
+            stock = 100
+            price = 2
+        }
+        banana = {
+            stock = 400
+            price = 5
+        }
+    }
+}
+
+## Access list variable, list index starts from zero
+output "fruits_first" {
+    value = var.fruits[0]
+}
+output "fruits_second" {
+    value = var.fruits[1]
+}
+
+## Access a Map variable
+
+output "fruit_stock_apple" {
+    value = var.fruit_stock["apple"]
+}
+
+output "fruit_stock_with_price_apple" {
+    value = var.fruit_stock_with_price["apple"].stock
+}
+
+#variable data types
+
+variable "fruit_details" {
+    default = {
+        apple = {
+            stock = 100   ##Number
+            type = "washington"    ##string
+            for_sale = true     ##Boolean
+        }
+    }
+}
+
+#variable in combination of any string it needs to be in ${}
+
+output "fruit_name_1" {
+    value = "Fruit Name = ${var.fruit_name}
+}
+
+output "fruit_details_apple" {
+    value = "Apple stock = ${var.fruit_details[apple].stock} , Apple Type = ${var.fruit_details[apple].type} , Apple sale status = ${var.fruit_details[apple].for_sale}"
+}
+
+
+    
