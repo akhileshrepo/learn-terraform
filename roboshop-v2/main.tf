d0d2c91550358fa8ca7401d45c481936a00e2e0a
AKhilesh <kothapallyakhil123@gmail.com>
2023-09-18 19:19:57 +0530

    functions and data-sources

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -19,7 +19,7 @@ variable "zone_id" {
 resource "aws_instance" "frontend" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
-  vpc_security_group_ids = "sg-0d3e4cc237236453f"
+  vpc_security_group_ids = ["sg-0d3e4cc237236453f"]
 
   tags = {
     Name = "frontend"
