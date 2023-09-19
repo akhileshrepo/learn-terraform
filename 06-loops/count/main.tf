54032d4f3bc4b44b9301701842bd72ab96554f62
AKhilesh <kothapallyakhil123@gmail.com>
2023-09-19 11:25:04 +0530

    added loops

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -4,9 +4,9 @@ variable "components" {
 
 resource "aws_instance" "instance" {
     count = length(var.components)
-    ami = ""
+    ami = "ami-03265a0778a880afb"
     instance_type = "t3.small"
-    vpc_security_group_ids = [""]
+    vpc_security_group_ids = ["sg-0d3e4cc237236453f"]
 
     tags = {
         Name = element(var.components, count.index)
