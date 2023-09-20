938f05f08c2dd2df129c9e3d433074f4249e4b3c
AKhilesh <kothapallyakhil123@gmail.com>
2023-09-20 12:26:55 +0530

    added roboshop

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -16,4 +16,17 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.instance.private_ip]
 }
 
+resource "null_resource" "ansible" {
+  depends_on = [
+    aws_route53_record.record
+  ]
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/roboshop-ansible
+git pull
+sleep 30
+ansible-playbook -i ${var.name}-dev.akhildevops.online,main.yml -e ansible_username=centos -e ansible_password=DevOps321 -e component=${var.name}
+EOF
+  }
+}
 
