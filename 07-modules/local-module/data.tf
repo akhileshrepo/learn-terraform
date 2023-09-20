b8323bce8ae3eba193f9ab1d28b906becf7d0ddb
AKhilesh <kothapallyakhil123@gmail.com>
2023-09-20 10:27:56 +0530

    added module

---
+++ 07-modules/local-module/data.tf
@@ -0,0 +1,9 @@
+data "aws_ami_ids" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-*"
+  owners           = ["973714476881"]
+}
+
+output "ami" {
+    value = data.aws_ami_ids.ami
+}
