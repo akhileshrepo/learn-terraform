0ffb335eedf362a035f07482de74881f33227064
AKhilesh <kothapallyakhil123@gmail.com>
2023-09-20 10:49:35 +0530

    added module

--- 07-modules/local-module/data.tf
+++ 07-modules/local-module/data.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex       = "centos-8-DevOps-practice"
+  name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
 }
 
