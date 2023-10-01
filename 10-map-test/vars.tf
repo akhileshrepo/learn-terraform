26a76f599eaf21f12205f010bf128b57be7d6bb2
AKhilesh <kothapallyakhil123@gmail.com>
2023-10-01 23:37:58 +0530

    added map-test

---
+++ 10-map-test/vars.tf
@@ -0,0 +1,37 @@
+variable "vpc" {
+    default = {
+        main = {
+            cidr = "10.0.0.0/16"
+            subnets = {
+                public = {
+                    public1 = {cidr = "10.0.0.0/16", az = "us-east1a"}
+                    public2 = {cidr = "10.0.1.0/16", az = "us-east1b"}
+                }
+                app = {
+                    app1 = {cidr = "10.0.2.0/16", az = "us-east1a"}
+                    app2 = {cidr = "10.0.3.0/16", az = "us-east1b"}
+                }
+                db = {
+                    db1 = {cidr = "10.0.4.0/16", az = "us-east1a"}
+                    db2 = {cidr = "10.0.5.0/16", az = "us-east1b"}
+                }
+            }
+        }
+    }
+}
+
+/*
+output "all_subnets" {
+    value = { for k,v in merge(var.vpc["main"]["subnets"]): k => v }
+}
+*/
+
+locals {
+    public = { for k,v in merge(var.vpc["main"]["subnets"]["public"]): k => v }
+    app = { for k,v in merge(var.vpc["main"]["subnets"]["app"]): k => v }
+    db = { for k,v in merge(var.vpc["main"]["subnets"]["db"]): k => v }
+}
+
+output "all_subnets" {
+    value = merge(local.public, local.app, local.db)
+}
