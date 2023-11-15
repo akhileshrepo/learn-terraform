4f7472c1ca9d28b3694c452346b318383314b5e1
AKhilesh <kothapallyakhil123@gmail.com>
2023-11-15 14:22:34 +0530

    updated code

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -1,20 +1,15 @@
 variable "components" {
-    default = [ "redis", "mongodb", "catalogue"]
+    default = ["frontend", "mongodb"]
 }
 
-resource "aws_instance" "instance" {
+resource aws_instance "instance" {
+
     count = length(var.components)
-    ami = "ami-03265a0778a880afb"
-    instance_type = "t3.small"
-    vpc_security_group_ids = ["sg-0d3e4cc237236453f"]
+    ami           = "ami-03265a0778a880afb"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["sg-0e9e01d2f78b0dd9a"]
 
     tags = {
         Name = element(var.components, count.index)
     }
-}
-
-
-resource "aws_security_group" "allow_tls" {
-    count = length(var.components)
-    name = element(var.components, count.index)
 }
