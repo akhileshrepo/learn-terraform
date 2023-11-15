12e5034a08d022a3fc429ca2a2883081480fee0e
AKhilesh <kothapallyakhil123@gmail.com>
2023-11-15 14:45:58 +0530

    updated code

--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -1,6 +1,6 @@
 variable "components" {
     default = {
-        frontend = { name = "frontend_sg"}
+        //frontend = { name = "frontend_sg"}
         catalogue = { name = "catalogue_sg"}
         mongodb = { name = "mongodb_sg"}
     }
