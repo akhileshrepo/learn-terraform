1a64ffce60d5828ecb985c38f56645d25a2e5464
AKhilesh <kothapallyakhil123@gmail.com>
2023-11-17 18:39:36 +0530

    updated code

--- roboshop-v3/vars.tf
+++ roboshop-v3/vars.tf
@@ -1,52 +1,52 @@
 variable "security_groups" {
-    default = ["sg-0dee954b08055e577"]
+    default = ["sg-0e9e01d2f78b0dd9a"]
 }
 
 variable "zone_id" {
-    default = "Z0021413JFIQEJP9ZO9Z"
+    default = "Z0929615AH1MSD5PXATC"
 }
 
 variable "components" {
     default = {
         frontend = {
             name          = "frontend"
-            instance_type = "t3.micro"
+            instance_type = "t2.micro"
         }
         cart = {
             name          = "cart"
-            instance_type = "t3.micro"
+            instance_type = "t2.micro"
         }
         mongodb = {
             name          = "mongodb"
-            instance_type = "t3.micro"
+            instance_type = "t2.micro"
         }
         catalogue = {
             name          = "catalogue"
-            instance_type = "t3.micro"
+            instance_type = "t2.micro"
         }
         user = {
             name          = "user"
-            instance_type = "t3.micro"
+            instance_type = "t2.micro"
         }
         redis = {
             name          = "redis"
-            instance_type = "t3.micro"
+            instance_type = "t2.micro"
         }
         mysql = {
             name          = "mysql"
-            instance_type = "t3.micro"
+            instance_type = "t2.micro"
         }
         payment = {
             name          = "payment"
-            instance_type = "t3.micro"
+            instance_type = "t2.micro"
         }
         shipping = {
             name          = "shipping"
-            instance_type = "t3.micro"
+            instance_type = "t2.micro"
         }
         rabbitmq = {
             name          = "rabbitmq"
-            instance_type = "t3.micro"
+            instance_type = "t2.micro"
         }
     }
 }
