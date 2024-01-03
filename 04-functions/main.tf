e06c46a707bded32b78f64ed4529a19b34195bf0
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-03 12:07:41 +0530

    updated code

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -2,12 +2,13 @@ variable "fruits" {
     default = ["apple", "banana"]
 }
 
+/*
 output "fruit" {
     //value = var.fruits[1]
     value = element(var.fruits, 5)
 }
+*/
 
-/*
 variable "fruit_with_stock" {
     default = {
         apple = {
@@ -20,6 +21,7 @@ output "fruit_stock" {
     value = try(var.fruit_with_stock["banana"], 0)
 }
 
+/*
 output "fruit_stock_price" {
     value = lookup(var.fruit_with_stock["apple"], "price", 2)
     // try(var.fruit_with_stock["apple"].price, 2)
