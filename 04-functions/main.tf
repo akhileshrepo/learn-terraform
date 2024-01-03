9112272d74e30dc885baa6ebf98253674920d759
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-03 12:09:48 +0530

    updated code

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -21,9 +21,9 @@ output "fruit_stock" {
     value = try(var.fruit_with_stock["banana"].stock, 0)
 }
 
-/*
+
 output "fruit_stock_price" {
     value = lookup(var.fruit_with_stock["apple"], "price", 2)
     // try(var.fruit_with_stock["apple"].price, 2)
 }
-*/
+
