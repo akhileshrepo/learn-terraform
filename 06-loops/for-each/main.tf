eb395d44217d3d276555e710a18192c26def9fff
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-03 16:52:39 +0530

    updated code

--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -9,5 +9,5 @@ variable "components" {
 
 resource "aws_security_group" "allow_tls" {
     for_each = var.components
-    name     = lookup(var.components, each.value["name"], null)
+    name     = lookup(each.value, "name", null)
 }
