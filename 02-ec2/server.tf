61f91b83e7e165aa374709bcb0743c654635d466
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-17 13:20:49 +0530

    updated code

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld from akhil"
