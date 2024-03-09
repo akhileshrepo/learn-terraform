d425f6d783596aae151318f04cf077df19b7332c
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-09 18:10:23 +0530

    updated code

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -11,11 +11,27 @@ resource "aws_instance" "instance" {
 }
 
 
-resource "aws_route53_record" "records" {
+resource "aws_route53_record" "record" {
   for_each = var.components
   zone_id  = var.zone_id
   name     = "${var.name}-dev.akhildevops.online"
   type     = "A"
   ttl      = 30
   records  = [aws_instance.instance.private_ip]
+}
+
+resource "null_resource" "ansible" {
+
+  depends_on = [
+    aws_route53_record.record
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/roboshop-ansible
+git pull
+sleep 30
+ansible-playbook -i ${var.name}-dev.akhildevops.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component = ${var.name}
+EOF
+  }
 }
