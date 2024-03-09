366bbb28ddd7ef8eedd41b14ce772d0b6be58792
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-09 13:19:13 +0530

    updated code

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -6,9 +6,9 @@ resource "aws_instance" "instance" {
 
   count = length(var.components)
 
-  ami           = "ami-03265a0778a880afb"
+  ami           = "ami-0f3c7d07486cad139"
   instance_type = "t3.small"
-  vpc_security_group_ids = [ "sg-0dee954b08055e577" ]
+  vpc_security_group_ids = [ "sg-0e9e01d2f78b0dd9a" ]
 
   tags = {
     //Name = var.components[count.index]
