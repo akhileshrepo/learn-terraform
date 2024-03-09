d86ae5ce5f472c62a4415fb34fbda01fae03a5cf
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-09 13:10:59 +0530

    updated code

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -2,21 +2,24 @@ variable "components" {
     default = ["frontend", "mongodb", "catalogue"]
 }
 
-//resource "aws_instance" "instance" {
-//
-//  count = length(var.components)
-//
-//  ami           = "ami-03265a0778a880afb"
-//  instance_type = "t3.small"
-//  vpc_security_group_ids = [ "sg-0dee954b08055e577" ]
-//
-//  tags = {
-//    //Name = var.components[count.index]
-//    Name = element(var.components, count.index)
-//  }
-//}
+resource "aws_instance" "instance" {
 
+  count = length(var.components)
+
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0dee954b08055e577" ]
+
+  tags = {
+    //Name = var.components[count.index]
+    Name = element(var.components, count.index)
+  }
+}
+
+
+/*
 resource "aws_security_group" "allow_tls" {
     count = length(var.components)
     name  = element(var.components, count.index)
 }
+*/
