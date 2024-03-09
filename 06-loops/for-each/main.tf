844c34892212c1c7859111b464b2de3e02eca100
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-09 13:52:46 +0530

    updated code

--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -1,5 +1,7 @@
 variable "components" {
     default = {
+        redis = { name= "redis_sg" }
+        catalogue = { name = "catalogue_sg" }
         frontend = { name = "frontend_sg" }
         mongodb   = { name = "mongodb_sg" }
     }
