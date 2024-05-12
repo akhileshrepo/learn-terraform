4f30a9b9bf9667a2ad6f8d1395fa18498d3523c6
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-12 10:28:03 +0530

    updated code

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -1,53 +1,31 @@
-variable "ami" {
-  default = "ami-0f3c7d07486cad139"
-}
+resource "aws_instance" "frontend" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group
 
-variable "security_groups" {
-  default = ["sg-0e9e01d2f78b0dd9a"]
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "zone_id" {
-  default = "Z0929615AH1MSD5PXATC"
-}
-
-variable "components" {
-  default = {
-    frontend = { name = "frontend-dev"}
-    mongodb = { name = "mongodb-dev"}
-    catalogue = { name = "catalogue-dev"}
-    redis = { name = "redis-dev"}
-    user = { name = "user-dev"}
-    cart = { name = "cart-dev"}
-    rabbitmq = { name = "rabbitmq-dev"}
-    mysql = { name = "mysql-dev"}
-    shipping = { name = "shipping-dev"}
-    payment = { name = "payment-dev"}
-  }
-}
-
-
-
-resource "aws_instance" "instance" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  vpc_security_group_ids = var.security_groups
-
-  for_each = var.components
+  for_each = each.value["components"]
 
   tags = {
     Name = lookup(each.value, "name", null)
   }
 }
 
-resource "aws_route53_record" "records" {
-  for_each = var.components
-  zone_id  = var.zone_id
-  name     = "${lookup(each.value, "name", null)}-dev.akhildevops.online"
-  type     = "A"
-  ttl      = 30
-  records  = [lookup(lookup(aws_instance.instance, each.key, null), "private_ip", null)]
+variable "ami" {
+  default = "ami-0f3c7d07486cad139"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "security_group" {
+  default = ["sg-0e9e01d2f78b0dd9a"]
+}
+
+variable "components" {
+  default = {
+    frontend = { name = "frontend-dev" }
+    mongodb  = { name = "mongodb-dev" }
+    catalogue = { name = "catalogue-dev" }
+  }
 }
