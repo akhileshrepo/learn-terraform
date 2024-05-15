0e31379d59f95a60a23dec7e8cfe38baf9ce026e
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-15 14:37:45 +0530

    updated code

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -10,7 +10,8 @@ output "fruit" {
 */
 
 output "apple_stock" {
-    value = try(var.fruit_with_stock["apple"].stock, 1000)
+#    value = try(var.fruit_with_stock["apple"].stock, 1000)
+    value = lookup(var.fruit_with_stock["apple"], "stock", 1000)
 }
 
 
