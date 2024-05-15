cd24b7da982bde7add440dd64ba11ba9e938222a
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-15 10:28:41 +0530

    updated code

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,9 +1,4 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "devops" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0e9e01d2f78b0dd9a"]
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
