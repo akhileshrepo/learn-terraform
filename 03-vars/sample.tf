b76adcdfd95d162d45c20e9e1771b6f3026f05ac
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-15 13:03:03 +0530

    updated code

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -1,90 +1,9 @@
-# plain variable
+# Plain variable
 
-variable "fruit" {
+variable "fruit_name" {
     default = "apple"
 }
 
-
-# List Variable
-variable "fruits" {
-    default = ["apple", "banana"]
-     //default = [ "apple", "banana" ] // Single line syntax
+output "print_fruit_name" {
+    value = var.fruit_name
 }
-
-
-
-# Map Variable , Plain
-variable "fruit_stock" {
-    default = {
-        apple  = 100
-        banana = 200
-    }
-}
-
-
-
-# Map Variable, Map of Maps
-variable "fruit_stock_with_price" {
-    default = {
-        apple = {
-            stock = 200
-            price = 3
-        }
-        banana = {
-            stock = 400
-            price = 1
-        }
-    }
-}
-
-
-/*
-## Access a list variable, List index starts from zero
-output "fruits_first" {
-    value = var.fruits[0]
-    // value = element(var.fruits, 0)
-}
-
-output "fruits_second" {
-    value = var.fruits[1]
-}
-
-
-
-## Access a Map Variable
-output "fruit_stock_apple" {
-    value = var.fruit_stock["apple"]
-}
-
-output "fruit_stock_with_price_of_apple" {
-    value = var.fruit_stock_with_price["apple"].stock
-}
-
-
-
-
-
-# Variable Data Types
-variable "fruit_details" {
-    default = {
-        apple = {
-            stock    = 100          # number
-            type     = "washington" # string
-            for_sale = true         # boolean
-        }
-    }
-}
-
-# Variable in a combination of any other string then it needs to be with in  ${}
-output "fruit_name_1" {
-    value = "Fruit Name = ${var.fruit_name}"
-}
-
-
-output "fruit_details_apple" {
-    value = "Apple Stock = ${var.fruit_details["apple"].stock} , Apple Type = ${var.fruit_details["apple"].type}, Apple Sale Status = ${var.fruit_details["apple"].for_sale}"
-}
-
-*/
-
-
--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -1,14 +1,14 @@
-resource "aws_instance" "frontend" {
-  for_each = var.components
-  ami           = var.ami
-  instance_type = var.instance_type
+resource "aws_instance" "instances" {
+  for_each        = var.components
+  ami             = var.ami
+  instance_type   = var.instance_type
   vpc_security_group_ids = var.security_group
-
 
   tags = {
     Name = lookup(each.value, "name", null)
   }
 }
+
 
 variable "ami" {
   default = "ami-0f3c7d07486cad139"
@@ -25,7 +25,5 @@ variable "security_group" {
 variable "components" {
   default = {
     frontend = { name = "frontend-dev" }
-    mongodb  = { name = "mongodb-dev" }
-    catalogue = { name = "catalogue-dev" }
   }
 }
