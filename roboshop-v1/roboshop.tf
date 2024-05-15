baa450cc0c27dd4cc80829fd20f2830ecf459485
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-15 10:47:37 +0530

    updated code

--- roboshop-v1/roboshop.tf
+++ roboshop-v1/roboshop.tf
@@ -1,17 +1,17 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0e9e01d2f78b0dd9a"]
 
   tags = {
-    Name = "frontend-server"
+    Name = "Frontend"
   }
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "www" {
   zone_id = "Z0929615AH1MSD5PXATC"
   name    = "frontend-dev.akhildevops.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.frontend.private_ip]
+  records = aws_instance.frontend.private_ip
 }
+
