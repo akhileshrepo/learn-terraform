c126952422cc0c26f1c2fdca180ad4c539458d84
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-15 13:15:36 +0530

    updated code

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -7,3 +7,39 @@ variable "fruit_name" {
 output "print_fruit_name" {
     value = var.fruit_name
 }
+
+# List variable
+
+variable "veg_name" {
+    default = [ "tomato", "potato", "onions"]
+}
+
+output "print_veg_name" {
+    value = var.veg_name
+}
+
+# Map variable - Plain
+
+variable "cooler_name" {
+    default = {
+        LG = 5000
+        sonata = 15000
+    }
+}
+
+output "print_LG_cooler_cost" {
+    value = var.cooler_name[LG]
+}
+
+# Map variable - Map of Maps
+
+variable "servers" {
+    default = {
+        Frontend = { name = "frontend", type = "t2.micro" }
+        Catalogue = { name = "catalogue", type = "t2.micro"}
+    }
+}
+
+output "print_server_frontend" {
+    value = var.servers[Frontend].name
+}
