771ff3e9145955b3237080b8a078a799c24141a4
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-16 20:50:32 +0530

    updated code

--- roboshop-v3/main.tf
+++ roboshop-v3/main.tf
@@ -1,13 +1,3 @@
-module "components" {
-
-  for_each = var.components
-
-  source          = "./module"
-  zone_id         = var.zone_id
-  security_groups = var.security_groups
-  name            = each.value["name"]
-  instance_type   = each.value["instance_type"]
-}
 
 
 
--- roboshop-v3/module/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "name" {}
-variable "zone_id" {}
-variable "instance_type" {}
-variable "security_groups" {}
