a8229055dfef648e337d90a40dd209244a6ddc4b
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-16 22:52:27 +0530

    updated code

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -19,14 +19,17 @@ resource "aws_route53_record" "record" {
 }
 
 resource "null_resource" "ansible" {
-  depends_on = [ aws_route53_record.record ]
+
+  depends_on = [
+    aws_route53_record.record
+  ]
 
   provisioner "local-exec" {
     command = <<EOF
 cd /home/centos/test
 git pull
 sleep 30
-ansible-playbook -i ${var.name}-dev.akhildevops.online main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+ansible-playbook -i ${var.name}-dev.rdevopsb72.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
 EOF
   }
 }
