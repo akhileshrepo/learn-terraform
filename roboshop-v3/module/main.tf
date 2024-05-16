b5af94f2303fc4ce443931efa4786127bcd68c7c
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-16 23:18:21 +0530

    updated code

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -29,7 +29,7 @@ resource "null_resource" "ansible" {
 cd /home/centos/test
 git pull
 sleep 30
-ansible-playbook -i ${var.name}-dev.rdevopsb72.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+ansible-playbook -i ${var.name}-dev.akhildevops.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
 EOF
   }
 }
