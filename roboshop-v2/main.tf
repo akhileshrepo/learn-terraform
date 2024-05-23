8df18bff84f0f72388f128c68393391c1c5bfb63
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-23 13:27:22 +0530

    updated code

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "instances" {
   for_each        = var.components
-  ami             = data.aws_ami_id.ami.id
+  ami             = data.aws_ami.ami.id
   instance_type   = lookup(each.value, "type", null)
   vpc_security_group_ids = var.vpc_security_group_ids
 
@@ -9,11 +9,12 @@ resource "aws_instance" "instances" {
   }
 }
 
-data "aws_ami_id" "ami" {
+data "aws_ami" "ami" {
   most_recent = true
-  name_regex = "Centos-*"
-  owners     = ["973714476881"]
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
 }
+
 
 variable "vpc_security_group_ids" {
   default = ["sg-0e9e01d2f78b0dd9a"]
