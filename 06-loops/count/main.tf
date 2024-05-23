1734d812fb5a39f8f14a54d7d27b59549c81430a
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-23 13:05:32 +0530

    updated code

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -1,22 +1,21 @@
 variable "components" {
-  default = {
-    frontend = {
-      name          = "frontend-dev"
-      instance_type = "t2.micro"
-    }
-    mongodb = {
-      name = "mongodb-dev"
-      instance_type = "t2.micro"
-    }
-  }
+  default = ["frontend", "mongodb", "catalogue"]
 }
 
 resource "aws_instance" "instances" {
-  for_each = var.components
-  ami           = "ami-0f3c7d07486cad139"
-  instance_type = lookup(each.value, "instance_type", null)
+  count         = length(var.components)
+  ami           = var.ami
+  instance_type = var.instance_type
 
   tags = {
-    Name = lookup(each.value, "name", null)
+    name = element(var.components, count.index)
   }
+}
+
+
+variable "ami" {
+  default = "ami-0f3c7d07486cad139"
+}
+variable "instance_type" {
+  default = "t2.micro"
 }
--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -10,3 +10,17 @@ resource "aws_security_group" "sg" {
     for_each = var.components
     name = lookup(each.value, "name", null)
 }
+
+resource "aws_instance" "instances" {
+    for_each      = var.components
+    ami           = var.ami
+    instance_type = lookup(each.value, "type", null)
+
+    tags = {
+        Name = lookup(each.value, "name", null)
+    }
+}
+
+variable "ami" {
+    default = "ami-0f3c7d07486cad139"
+}
