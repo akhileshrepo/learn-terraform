f3bcc7fc1c8bf396d547b329c44c862961768456
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-23 12:50:26 +0530

    updated code

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -2,7 +2,7 @@ variable "components" {
   default = {
     frontend = {
       name = "frontend-dev"
-      type = "t2.micro"
+      instance_type = "t2.micro"
     }
     mongodb = {
       name = "mongodb-dev"
@@ -14,7 +14,7 @@ variable "components" {
 resource "aws_instance" "instances" {
   for_each = var.components
   ami           = "ami-0f3c7d07486cad139"
-  instance_type = lookup(var.components, each.value["type"], null)
+  instance_type = lookup(var.components, each.value["instance_type"], null)
 
   tags = {
     Name = lookup(var.components, each.value["name"], null)
