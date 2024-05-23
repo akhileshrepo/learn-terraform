e242bdcf6ef16849c86374c6c33ae7034d036d57
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-23 13:16:25 +0530

    updated code

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -1,58 +1,37 @@
 resource "aws_instance" "instances" {
-  for_each      = var.components
-  ami           = data.aws_ami.ami
-  instance_type = lookup(each.value, "instance_type", null)
-  vpc_security_group_ids = var.security_groups
+  for_each        = var.components
+  ami             = data.aws_ami_ids.ami
+  instance_type   = lookup(each.value, "type", null)
+  vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
     name = lookup(each.value, "name", null)
   }
 }
 
-resource "aws_route53_record" "records" {
-  for_each = var.components
-  name    = "${lookup(each.value, "name", null)}.akhildevops.online"
-  type    = "A"
-  zone_id = var.zone_id
-  ttl     = 30
-  records = [lookup(lookup(aws_instance.instances, each.key), "private_ip", null)]
-}
-
-
-data "aws_ami" "ami" {
+data "aws_ami_ids" "ami" {
   most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
+  name_regex = "Centos-*"
+  owners     = ["973714476881"]
 }
 
-
-
-
-
-variable "zone_id" {
-  default = "Z0929615AH1MSD5PXATC"
-}
-
-
-
-
-
-variable "ami" {
-  default = "ami-0f3c7d07486cad139"
-}
-
-variable "security_groups" {
+variable "vpc_security_group_ids" {
   default = ["sg-0e9e01d2f78b0dd9a"]
 }
+
 variable "components" {
   default = {
-    frontend = {
-      name = "frontend-dev"
-      instance_type = "t2.micro"
+    frontend ={
+      name = frontend-dev
+      type = "t2.micro"
     }
-    mongodb = {
-      name = "mongodb-dev"
-      instance_type = "t2.micro"
+    mongodb ={
+      name = mongodb-dev
+      type = "t2.micro"
+    }
+    catalogue ={
+      name = catalogue-dev
+      type = "t2.micro"
     }
   }
 }
