89aebf0c979f57a63b8e695d94546201bbe642c1
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-20 17:54:27 +0530

    updated code

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -3,11 +3,16 @@ resource "aws_instance" "test" {
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
 
+
   tags = {
     Name = "Akhilesh"
   }
 }
 
-data "aws_instance" "test" {
-  private_ip = aws_instance.test.private_ip
+resource "aws_route53_record" "test" {
+  zone_id = "Z093842334KRCLE5WWCFA"
+  name    = "frontend-dev.akhildevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.test.private_ip]
 }
