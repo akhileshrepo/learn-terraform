7c2379ace924a0ea14a1553f08f6ad08673482cf
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-20 18:20:22 +0530

    updated code

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,17 +1,35 @@
-resource "aws_instance" "test" {
+resource "aws_instance" "frontend" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
 
   tags = {
-    Name = "Akhilesh"
+    Name = "frontend"
   }
 }
 
-resource "aws_route53_record" "test" {
+resource "aws_route53_record" "frontend" {
   zone_id = "Z093842334KRCLE5WWCFA"
   name    = "frontend-dev.akhildevops.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.test.private_ip]
+  records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z093842334KRCLE5WWCFA"
+  name    = "mongodb-dev.akhildevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
 }
