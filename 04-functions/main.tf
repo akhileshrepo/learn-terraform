929caa6432636834d2549797f8e27164863ac19d
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-21 14:55:05 +0530

    updated code

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -1,3 +1,5 @@
+# element Function will help in replacing the list values
+
 variable "fruits" {
     default = [ "apple", "pineapple" ]
 }
@@ -7,21 +9,23 @@ output "second_fruit" {
     value = element(var.fruits, 5)
 }
 
+# lookup Function will help in replacing the map values
 
+variable "fruits_stock_with_price" {
+    default = {
+        apple = {
+            stock = 100
+            price = 50
+        }
+        orange = {
+            stock = 600
+            price = 40
+        }
+    }
+}
 
-# variable "laptop" {
-#     default = {
-#         samsung = {
-#             name = "samsung-dev", type = "andriod", model = 2015
-#         }
-#         lenovo = {
-#             name = "lenovo-dev", type = "snapdragon", model = 2018
-#         }
-#     }
-# }
-#
-# output "laptop" {
-# #    value = lookup(lookup(var.laptop, "samsung", null), "type", null)
-#     value = try(var.laptop["samsung"].type, null)
-# }
+output "apple_stock" {
+#     value = var.fruits_stock_with_price["apple"].stock
+    value = lookup(lookup(var.fruits_stock_with_price, "apple", null), "stock", null)
+}
 
