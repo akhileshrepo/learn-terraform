f10b3033ed4b4adeb827b0a59f4835c361c8a98d
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-21 15:35:40 +0530

    updated code

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -1,21 +1,14 @@
 variable "components" {
-  default = ["frontend", "mongodb", "catalogue"]
+  default = [ "frontend", "mongodb", "catalogue" ]
 }
 
-resource "aws_instance" "instances" {
+resource "aws_instance" "web" {
   count         = length(var.components)
-  ami           = var.ami
-  instance_type = var.instance_type
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
 
   tags = {
-    name = element(var.components, count.index)
+    name = var.components[count.index]
+#     name = element(var.components, count.index)
   }
-}
-
-
-variable "ami" {
-  default = "ami-0f3c7d07486cad139"
-}
-variable "instance_type" {
-  default = "t2.micro"
 }
