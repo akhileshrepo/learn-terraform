e5f8c8a59cd3b92760c39ab0856ebf1e51c37ce2
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-21 15:18:07 +0530

    updated code

--- 05-data-sources/main.tf
+++ 05-data-sources/main.tf
@@ -1,9 +1,8 @@
 data "aws_ami_id" "ami" {
-  most_recent = true
   name_regex = "Centos-*"
   owners     = ["973714476881"]
 }
 
 output "ami" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami_id.ami
 }
