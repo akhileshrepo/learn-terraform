1d38554a3d2b2e5f1aee10ab1ea7593c0fdd8d68
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-21 15:46:39 +0530

    updated code

--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -1,26 +1,26 @@
 variable "components" {
     default = {
-        catalogue = { name = "catalogue-dev", type = "t2.micro"}
-        mongodb = { name = "mongodb-dev", type = "t2.micro"}
-        frontend  = { name = "frontend-dev", type = "t2.micro" }
+        frontend = {
+            name = "frontend-dev"
+            instance_type = "t2.micro"
+        }
+        mongodb = {
+            name = "mongodb-dev"
+            instance_type = "t2.micro"
+        }
+        catalogue = {
+            name = "catalogue-dev"
+            instance_type = "t2.micro"
+        }
     }
 }
 
-resource "aws_security_group" "sg" {
+resource "aws_instance" "web" {
+    ami           = "ami-0b4f379183e5706b9"
+    instance_type = "t2.micro"
     for_each = var.components
-    name = lookup(each.value, "name", null)
-}
-
-resource "aws_instance" "instances" {
-    for_each      = var.components
-    ami           = var.ami
-    instance_type = lookup(each.value, "type", null)
 
     tags = {
-        Name = lookup(each.value, "name", null)
+        name = lookup(each.value, "name", null)
     }
-}
-
-variable "ami" {
-    default = "ami-0f3c7d07486cad139"
 }
