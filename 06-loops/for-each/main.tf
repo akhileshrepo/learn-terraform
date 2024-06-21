0e35bdf0d07fb62688791ac05279ed9c31f4e782
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-21 16:00:10 +0530

    updated code

--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -16,19 +16,12 @@ variable "components" {
 }
 
 resource "aws_instance" "web" {
+    for_each = var.components
     ami           = "ami-0b4f379183e5706b9"
     instance_type = "t2.micro"
-    for_each = var.components
+
 
     tags = {
         name = lookup(each.value, "name", null)
-    }
-}
-
-resource "aws_security_group" "allow_tls" {
-    for_each    = var.components
-
-    tags = {
-        Name = lookup(each.value, "name", null)
     }
 }
