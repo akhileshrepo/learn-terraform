8f5f313fb592ed9762c9d23ba388cfe61beb4bb3
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-21 14:40:55 +0530

    updated code

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -1,25 +1,26 @@
-variable "fruit_name" {
-    default = ["apple", "pineapple"]
+variable "fruits" {
+    default = [ "apple", "pineapple" ]
 }
 
-output "fruit_name" {
-    value = var.fruit_name[1]
-    #value = element(var.fruit_name, 2)
+output "second_fruit" {
+    value = element(var.fruits, 8)
 }
 
-variable "laptop" {
-    default = {
-        samsung = {
-            name = "samsung-dev", type = "andriod", model = 2015
-        }
-        lenovo = {
-            name = "lenovo-dev", type = "snapdragon", model = 2018
-        }
-    }
-}
 
-output "laptop" {
-#    value = lookup(lookup(var.laptop, "samsung", null), "type", null)
-    value = try(var.laptop["samsung"].type, null)
-}
+
+# variable "laptop" {
+#     default = {
+#         samsung = {
+#             name = "samsung-dev", type = "andriod", model = 2015
+#         }
+#         lenovo = {
+#             name = "lenovo-dev", type = "snapdragon", model = 2018
+#         }
+#     }
+# }
+#
+# output "laptop" {
+# #    value = lookup(lookup(var.laptop, "samsung", null), "type", null)
+#     value = try(var.laptop["samsung"].type, null)
+# }
 
