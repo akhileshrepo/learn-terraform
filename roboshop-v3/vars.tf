eda95c028c95900dd8279fb79709391d5ddf1e64
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-22 12:59:58 +0530

    updated code

--- roboshop-v3/vars.tf
+++ roboshop-v3/vars.tf
@@ -9,43 +9,43 @@ variable "components" {
   default = {
     frontend = {
       name = "frontend"
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
     mongodb = {
       name = "mongodb"
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
     catalogue = {
       name = "catalogue"
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
     redis = {
       name = "redis"
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
     user = {
       name = "user"
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
     cart = {
       name = "cart"
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
     mysql = {
       name = "mysql"
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
     shipping = {
       name = "shipping"
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
     rabbitmq = {
       name = "rabbitmq"
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
     payment = {
       name = "payment"
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
   }
 }
