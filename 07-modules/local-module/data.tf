362ed407698efbd023f064dbcf630c3e1e32e576
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-22 11:40:40 +0530

    updated code

--- 07-modules/local-module/data.tf
+++ 07-modules/local-module/data.tf
@@ -1,4 +1,4 @@
-data "aws_ami_ids" "ami" {
+data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
@@ -6,14 +6,11 @@ data "aws_ami_ids" "ami" {
 
 
 resource "aws_instance" "instances" {
-  ami           = data.aws_ami_ids.ami
+  ami           = data.aws_ami.ami
   instance_type = var.instance_type
   vpc_security_group_ids = var.vpc_security_group_ids
 }
 
 variable "instance_type" {}
-
-
-
 variable "vpc_security_group_ids" {}
 
--- 07-modules/main.tf
+++ 07-modules/main.tf
@@ -1,14 +1,13 @@
 module "test" {
-    source    = "./local-module"
-    vpc_security_group_ids  = var.vpc_security_group_ids
-    instance_type           = var.instance_type
+    source = "./local-module"
+    instance_type = var.instance_type
+    vpc_security_group_ids = var.vpc_security_group_ids
 }
 
 variable "vpc_security_group_ids" {
-    default = ["sg-0e9e01d2f78b0dd9a"]
+    default = ["sg-0ad8ec6873fafd140"]
 }
 
 variable "instance_type" {
     default = "t2.micro"
 }
-
