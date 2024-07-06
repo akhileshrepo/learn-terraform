035e1256402474f4b6daacf33e6da8e1f9b88775
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-06 13:28:09 +0530

    updated code

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,35 +1,8 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "web" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
 
   tags = {
-    Name = "frontend"
+    Name = "Terraform"
   }
-}
-
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z093842334KRCLE5WWCFA"
-  name    = "frontend-dev.akhildevops.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
-
-resource "aws_instance" "mongodb" {
-  ami           = "ami-0b4f379183e5706b9"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z093842334KRCLE5WWCFA"
-  name    = "mongodb-dev.akhildevops.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
 }
