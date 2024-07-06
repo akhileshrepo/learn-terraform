927399d6f942f5c9c95a38e053dff3ebd8bbabd4
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-06 14:18:38 +0530

    updated code

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -44,37 +44,40 @@ output "orange" {
     value = var.fruit_stock["orange"]
 }
 
-# # Map of maps variable and Accessing the Map of Maps
-#
-# variable "fruit_stock_with_price" {
-#     default = {
-#         apple = {
-#             stock = 100
-#             price = 50
-#         }
-#         pineapple = {
-#             stock = 500
-#             price = 40
-#         }
-#         orange = {
-#             stock = 500
-#             price = 30
-#         }
-#     }
-# }
-#
-# output "apple_price" {
-#     value = var.fruit_stock_with_price["apple"].price
-# }
-#
-# output "pineapple_price" {
-#     value = var.fruit_stock_with_price["pineapple"].price
-# }
-#
-# output "orange_price" {
-#     value = var.fruit_stock_with_price["orange"].price
-# }
-#
+# Map of maps variable and Accessing the Map of Maps
+
+variable "Fruit_message" {
+    default = {
+        apple = {
+            stock = 100
+            type  = "washington"
+            for_sale = true
+        }
+        orange = {
+            stock = 150
+            type = "assam"
+            for_sale = true
+        }
+        pineapple = {
+            stock = 250
+            type  = "delhi"
+            for_sale = true
+        }
+    }
+}
+
+output "Fruit_message_apple" {
+    value = var.Fruit_message["apple"].type
+}
+
+output "Fruit_message_orange" {
+    value = var.Fruit_message["orange"].type
+}
+
+output "Fruit_message_pineapple" {
+    value = var.Fruit_message["pineapple"].type
+}
+
 # # variable Data types - Accessing variable Data type
 #
 # variable "data_type" {
