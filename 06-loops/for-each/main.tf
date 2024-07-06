48200593575f3680f421bf85ee9974a6431e64fe
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-06 15:28:22 +0530

    updated code

--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -1,35 +1,25 @@
-resource "aws_instance" "instances" {
-    for_each      = var.components
-    ami           = "ami-0b4f379183e5706b9"
-    instance_type = "t2.micro"
-
-    tags = {
-        Name = lookup(each.value, "name", null)
-    }
-}
-
 variable "components" {
     default = {
         frontend = {
-            name = "frontend-dev"
             instance_type = "t2.micro"
         }
         mongodb = {
-            name = "mongodb-dev"
             instance_type = "t2.micro"
         }
         catalogue = {
-            name          = "catalogue-dev"
             instance_type = "t2.micro"
         }
     }
 }
 
-resource "aws_security_group" "allow_tls" {
-    for_each = var.components
-    name = lookup(each.value, "name", null)
+resource "aws_instance" "instance" {
+    for_each      = var.components
+    ami           = "ami-0b4f379183e5706b9"
+    instance_type = each.value["instance_type"]
+    vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
 
     tags = {
-        Name = lookup(each.value, "name", null)
+        Name = each.key
     }
 }
+
