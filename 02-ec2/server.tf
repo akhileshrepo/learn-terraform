29706c02885e830e0ae914d73f0f67890a11ac98
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-06 13:32:34 +0530

    updated code

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "instance" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
 
   tags = {
     Name = "Terraform"
