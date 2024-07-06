45d008f96c9892742be87bb97a9c0d4283b150f0
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-06 14:47:07 +0530

    updated code

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -1,31 +1,30 @@
 # element Function will help in accessing the list values
 
-variable "fruits" {
-    default = [ "apple", "pineapple" ]
+variable "Fruits" {
+    default = ["apple", "orange", "pineapple"]
 }
 
-output "second_fruit" {
-#     value = var.fruits[3]
-    value = element(var.fruits, 5)
+output "Fruit_apple" {
+    value = element(var.Fruits, 5)
 }
 
-# lookup Function will help in accessing the map values
-
-variable "fruits_stock_with_price" {
-    default = {
-        apple = {
-            stock = 100
-            price = 50
-        }
-        orange = {
-            stock = 600
-            price = 40
-        }
-    }
-}
-
-output "apple_stock" {
-#     value = var.fruits_stock_with_price["apple"].stock
-    value = lookup(lookup(var.fruits_stock_with_price, "apple", null), "stock", null)
-}
+# # lookup Function will help in accessing the map values
+#
+# variable "fruits_stock_with_price" {
+#     default = {
+#         apple = {
+#             stock = 100
+#             price = 50
+#         }
+#         orange = {
+#             stock = 600
+#             price = 40
+#         }
+#     }
+# }
+#
+# output "apple_stock" {
+# #     value = var.fruits_stock_with_price["apple"].stock
+#     value = lookup(lookup(var.fruits_stock_with_price, "apple", null), "stock", null)
+# }
 
