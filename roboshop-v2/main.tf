cee6c3104ee7b9e7bc3bbb3a5dd71ba7ee72c600
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-14 09:23:27 +0530

    changes

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -23,23 +23,43 @@ resource "aws_route53_record" "record" {
 variable "components" {
   default = {
     frontend = {
-      name = "frontend-dev"
+      name = "frontend"
       instance_type = "t2.micro"
     }
     mongodb = {
-      name = "mongodb-dev"
+      name = "mongodb"
       instance_type = "t2.micro"
     }
     catalogue = {
-      name = "catalogue-dev"
+      name = "catalogue"
       instance_type = "t2.micro"
     }
     redis = {
-      name = "redis-dev"
+      name = "redis"
       instance_type = "t2.micro"
     }
     user = {
-      name = "user-dev"
+      name = "user"
+      instance_type = "t2.micro"
+    }
+    cart = {
+      name = "cart"
+      instance_type = "t2.micro"
+    }
+    shipping = {
+      name = "shipping"
+      instance_type = "t2.micro"
+    }
+    mysql = {
+      name = "mysql"
+      instance_type = "t2.micro"
+    }
+    rabbitmq = {
+      name = "rabbitmq"
+      instance_type = "t2.micro"
+    }
+    payment = {
+      name = "payment"
       instance_type = "t2.micro"
     }
   }
